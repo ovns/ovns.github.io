39d46eb7ab506d332b1e51ebe29b42f3a2da8057
Apostolis Xekoukoulotakis <apostolis.xekoukoulotakis@gmail.com>
2014-11-13 22:39:52 +0000

    Wiki links now link to the actual .md files (despite the fact that they are redirected to a page with the markdown interpreted.)

--- github-wiki.js
+++ github-wiki.js
@@ -13,7 +13,7 @@ if (document.URL.match("=")) {
 //transforms double brackets links into markdown links
 function link_converter(md) {
     return md.replace(/\[\[([\w\s\-]+)\]\]/g, function(match, p1, offset, string) {
-        return "[" + p1 + "](/" + p1.replace(/\s/g, "_") + ")";
+        return "[" + p1 + "](/wiki/" + p1.replace(/\s/g, "_") + ".md)";
     });
 }
 
@@ -26,8 +26,7 @@ function load_article(title) {
     $.ajax({
         url: "wiki/" + title + ".md",
         success: function(data) {
-            var lmd = link_converter("# "+title.replace(/_/g," ")+"\n\n"+data);
-            console.log(lmd);
+            var lmd = link_converter("# " + title.replace(/_/g, " ") + "\n\n" + data);
             var html = converter.makeHtml(lmd);
             $("#article").empty();
             $("#article").append(html);
@@ -36,7 +35,7 @@ function load_article(title) {
             $("a").on("click", function(e) {
                 var link = $(e.target).attr("href");
                 if (link[0] == "/" && link[1] != "/" && link.match(".html") == null) {
-                    window.location.href = "/wiki.html?article=" + link.substr(1);
+                    window.location.href = "/wiki.html?article=" + link.substring(6, link.length - 3);
                     return false;
                 }
             });
